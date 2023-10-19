f0cd710762b07658961d5bb63e11f3c949588f67
aaron <afarrell002@gmail.com>
2023-10-18 19:41:41 -0600

    created base for trending page and trending stylesheet

---
+++ react-video-app/src/pages/Trending.jsx
@@ -0,0 +1,12 @@
+import Wrapper from "../styles/Trending";
+
+function TrendingPage() {
+  return (
+    <Wrapper>
+      <h2>Trending Videos</h2>
+      <div className="trending">{/* Display Trending Videos */}</div>
+    </Wrapper>
+  );
+}
+
+export default TrendingPage;
