5fb4d447357dc4b12c6cb5f4568f538a97818b28
aaron <afarrell002@gmail.com>
2023-10-18 19:40:12 -0600

    created base for home page and home stylesheet

---
+++ react-video-app/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Wrapper from "../styles/Home";
+import VideoGrid from "../styles/VideoGrid";
+
+function Home() {
+  return (
+    <Wrapper>
+      <VideoGrid></VideoGrid>
+    </Wrapper>
+  );
+}
